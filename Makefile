cf63cd837986b4ff6bb08cf593f07aaad043b2f5
C F <cf@MacBook-Pro-3.local>
2024-01-25 15:39:09 -0500

    added merge and quick sort

--- Makefile
+++ Makefile
@@ -1,22 +1,31 @@
 .PHONY: all clean
 
-all: sort1 sort2 experiment
+all: sort1 sort2 sort3 optimized experiment
 
 sort1: bubble_sort.py
 	echo '#!/usr/bin/env python3' > sort1
 	cat bubble_sort.py >> sort1
 	chmod +x sort1
 
-sort2: optimized.py
+sort2: quick_sort.py
 	echo '#!/usr/bin/env python3' > sort2
-	cat optimized.py >> sort2
+	cat sort2.py >> sort2
 	chmod +x sort2
+
+sort3: merge_sort.py
+	echo '#!/usr/bin/env python3' > sort3
+	cat sort3.py >> sort2
+	chmod +x sort3
+
+optimized: optimized.py
+	echo '#!/usr/bin/env python3' > optimized
+	cat optimized.py >> sort2
+	chmod +x optimized
 
 experiment: experiment.py
 	echo '#!/usr/bin/env python3' > experiment
 	cat experiment.py >> experiment
 	chmod +x experiment
-
 
 clean:
 	rm -f sort1 sort2 experiment
--- experiment.py
+++ experiment.py
@@ -1,45 +1,9 @@
-# import time
-# import random
-# import subprocess
-
-# def generate_random_input_file(size):
-#     with open("inputfile.csv", "w") as file:
-#         for _ in range(size):
-#             sort_key = random.randint(1, 1000)
-#             values = [random.randint(1, 100) for _ in range(10)]  # Adjust the range and number of values as needed
-#             row = [str(sort_key)] + list(map(str, values))
-#             file.write(','.join(row) + '\n')
-
-# def measure_runtime(script, input_size):
-#     #generate_random_input_file(input_size)
-
-#     start_time = time.time()
-#     subprocess.run([script, "inputfile.csv"])
-#     end_time = time.time()
-
-#     return end_time - start_time
-
-# if __name__ == "__main__":
-#     input_sizes = [10, 50, 100, 200, 500, 1000, 5000, 7500, 10000]  # Adjust the input sizes as needed
-
-#     for input_size in input_sizes:
-#         runtime_bubble_sort = measure_runtime("./bubble_sort.py", input_size)
-#         runtime_optimized_bubble_sort = measure_runtime("./optimized.py", input_size)
-
-#         print(f"Input Size: {input_size}")
-#         print(f"Bubble Sort Runtime: {runtime_bubble_sort:.6f} seconds")
-#         print(f"Optimized Bubble Sort Runtime: {runtime_optimized_bubble_sort:.6f} seconds")
-#         print()
+#!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
 import time
 import subprocess
 import random
-
-import numpy as np
-# Function to calculate n^2 for given input sizes
-def n_squared(input_sizes):
-    return np.square(input_sizes)
 
 
 def generate_random_input_file(size):
@@ -50,6 +14,7 @@ def generate_random_input_file(size):
             row = [str(sort_key)] + list(map(str, values))
             file.write(','.join(row) + '\n')
 
+
 def measure_runtime(script, input_size):
     generate_random_input_file(input_size)
 
@@ -58,6 +23,7 @@ def measure_runtime(script, input_size):
     end_time = time.time()
 
     return end_time - start_time
+
 
 if __name__ == "__main__":
     input_sizes = [10, 50, 100, 200, 500, 1000, 5000, 7500, 10000]  # Adjust the input sizes as needed
@@ -76,7 +42,6 @@ if __name__ == "__main__":
         print(f"Bubble Sort Runtime: {runtime_bubble_sort:.6f} seconds")
         print(f"Optimized Bubble Sort Runtime: {runtime_optimized_bubble_sort:.6f} seconds")
         print()
-
 
     # Plotting
     plt.plot(input_sizes, bubble_sort_runtimes, label='Bubble Sort')
